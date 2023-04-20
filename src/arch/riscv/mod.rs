e214c1b718eb4d98f1713c0b5468c23155fc6e64
kuangjux <18630816527@163.com>
2023-04-20 22:19:55 +0800

    add vcpu

--- src/arch/riscv/mod.rs
+++ src/arch/riscv/mod.rs
@@ -2,4 +2,5 @@ mod ept;
 mod guest;
 mod vcpu;
 
+pub use ept::GuestPageTableSv39 as ArchGuestPageTable;
 pub use guest::Guest;
---
+++ src/arch/riscv/vcpu.rs
@@ -0,0 +1,54 @@
+use alloc::sync::Arc;
+use riscv::register::hstatus::{self, Hstatus};
+use riscv::register::sstatus::{self, Sstatus, SPP};
+
+use super::Guest;
+
+pub struct TrapContext {
+    /// general regs[0..31]
+    pub x: [usize; 32],
+    /// CSR sstatus
+    pub sstatus: Sstatus,
+    /// CSR sepc
+    pub sepc: usize,
+    /// CSR hgatp
+    pub hgatp: usize,
+    /// CSR hstatus
+    pub hstatus: Hstatus,
+    /// kernel stack
+    pub kernel_sp: usize,
+    /// Addr of trap_handler function
+    pub trap_handler: usize,
+}
+
+pub struct VCpu {
+    pub trap_cx: TrapContext,
+    pub guest: Arc<Guest>,
+}
+
+impl VCpu {
+    pub fn new(
+        entry: usize,
+        sp: usize,
+        hgatp: usize,
+        kernel_sp: usize,
+        trap_handler: usize,
+        guest: Arc<Guest>,
+    ) -> Self {
+        let mut sstatus = sstatus::read();
+        sstatus.set_spp(SPP::Supervisor);
+        let mut hstatus = hstatus::read();
+        hstatus.set_spv(true);
+        let mut trap_cx = TrapContext {
+            x: [0; 32],
+            sstatus,
+            sepc: entry,
+            hgatp,
+            hstatus,
+            kernel_sp,
+            trap_handler,
+        };
+        trap_cx.x[2] = sp;
+        Self { trap_cx, guest }
+    }
+}
